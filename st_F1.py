4dd9347ff2ad53e8302dc7c936b9ca9787b04e06
Somu-uchisa <87493457+Somu-uchisa@users.noreply.github.com>
2022-03-14 15:30:17 +0530

    Update st_F1.py

--- st_F1.py
+++ st_F1.py
@@ -1,39 +1,4 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-ra=st.sidebar.radio('Select an option',['Analysis','Prediction'])
-if ra == "Analysis":
-     st.title('Water Quality Analysis')
-     st.image("water.jpg")
-
-    #    "Analysis"
-    rad = st.sidebar.selectbox('Select a State', ['Andhra Pradesh', 'TamilNadu'])
-
-    # This is for Ap data
-    if rad == 'Andhra Pradesh':
-        st.subheader("Andhra Pradesh Water Analysis")
-        df = pd.read_csv("C:/Users/somas/OneDrive/Desktop/F_WQI_Ap_jan_2021.csv")
-        # st.write(df)
-        if st.checkbox('View dataset'):
-            st.write(df)
-
-        st.bar_chart(df)
-
-    # This is for Tamilnadu
-    if rad == 'TamilNadu':
-        st.subheader("TamilNadu Water Analysis")
-        df = pd.read_csv("C:/Users/somas/OneDrive/Desktop/F_WQI_Ap_jan_2021.csv")
-        # st.write(df)
-        if st.checkbox('View dataset'):
-            st.write(df)
-        st.line_chart(df)
-
-if ra== "Prediction":
-    st.title('Water Quality Prediction')
-    st.image("pre.jpg")
-    
-    st.write('Prediction')
+   st.write('Prediction')
     pre = st.sidebar.selectbox('Select a State', ['KNN', 'SVM','Random Forest','Desicion tress'])
 
     if pre == 'KNN':
